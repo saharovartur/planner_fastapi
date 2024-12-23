1c935d4f20642cdba8078bbc4e7ee62bd15e5efa
Artur Sakharov <108172893+saharovartur@users.noreply.github.com>
2024-12-23 10:28:37 +0400

    добавил CRD для дел, добавил новый маршрту для обработки дел

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+
+from routes.events import event_router
 from routes.users import user_router
 
 import uvicorn
@@ -6,6 +8,7 @@ import uvicorn
 app = FastAPI()
 
 app.include_router(user_router, prefix="/user")
+app.include_router(event_router, prefix="/event")
 
 if __name__ == '__main__':
     uvicorn.run("main:app", host='0.0.0.0', port=8000, reload=True)
