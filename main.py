c5d8757c617b39d63d329778052b8d13c7fd2f66
Artur Sakharov <108172893+saharovartur@users.noreply.github.com>
2024-12-23 15:49:06 +0400

    добавил функцию для запуска функции из файла с настройками БД

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 
+from database.connection import conn
 from routes.events import event_router
 from routes.users import user_router
 
@@ -9,6 +10,12 @@ app = FastAPI()
 
 app.include_router(user_router, prefix="/user")
 app.include_router(event_router, prefix="/event")
+
+
+@app.on_event("startup")
+def on_startup():
+    conn()
+
 
 if __name__ == '__main__':
     uvicorn.run("main:app", host='0.0.0.0', port=8000, reload=True)
