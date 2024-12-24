d3c9d974520cdeb6f85b6bcd2aa6d19a1c389553
Artur Sakharov <108172893+saharovartur@users.noreply.github.com>
2024-12-24 13:07:30 +0400

    обновлены модели

--- models/events.py
+++ models/events.py
@@ -1,14 +1,37 @@
+from beanie import Document
+from typing import List, Optional
 from pydantic import BaseModel
-from typing import List
 
 
-class Event(BaseModel):
-    id: int
+class Event(Document):
     title: str
     image: str
     description: str
     tags: List[str]
     location: str
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "title": 'Learn fastAPI',
+                "image": "https://linktomyimage.com/image.png",
+                "description": "We will discuss fastapi",
+                "tags": ["python", "fastapi", "web", "backend"],
+                "location": "Zoom"
+
+            }
+        }
+
+    class Settings:
+        name = "events"
+
+
+class EventUpdate(BaseModel):
+    title: Optional[str]
+    image: Optional[str]
+    description: Optional[str]
+    tags: Optional[List[str]]
+    location: Optional[str]
 
     class Config:
         schema_extra = {
